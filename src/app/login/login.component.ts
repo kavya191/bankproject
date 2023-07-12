d0068516ac610e0ef80d7f2d8de4cbc290551a3c
kavya191 <kavyabose191@gmail.com>
2023-07-12 11:15:02 +0530

    bank

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,22 @@ export class LoginComponent  {
   data="Happy Banking with us"
   accountNo="Enter Acc No..."
   userPass="Enter Password..."
+  acc:any=""
+  pass:any=""
 
 
   login(){
     alert("Logged In")
+  }
+
+  accnoChange(event:any){
+    this.acc=event.target.value
+    console.log(this.acc);
+    
+  }
+  passChange(event:any){
+    this.pass=event.target.value
+    console.log(this.pass);
   }
 
 }
